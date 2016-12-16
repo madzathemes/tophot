dc3b4e9de217f97edfe0c77172da7b09e9e29806
Madars Bitenieks <madza@madzathemes.com>
2016-12-16 21:53:09 +0200

    fix

--- inc/js/effects.js
+++ inc/js/effects.js
@@ -186,3 +186,52 @@ d.slice(e-c+1,e+c+2).addClass("slick-active").attr("aria-hidden","false")),0===a
     }
 
 })(window.jQuery || window.Zepto || window.$, window);
+
+jQuery(document).ready(function() {
+  'use strict';
+
+  jQuery('.post-gallery').slick({
+     arrows: false,
+     lazyLoad: 'ondemand',
+     asNavFor: '.post-gallery-nav',
+     adaptiveHeight: true,
+     rtl: $rtl,
+  });
+  jQuery('.post-gallery-nav').slick({
+     slidesToShow: 5,
+     asNavFor: '.post-gallery',
+     centerPadding: '20px',
+     focusOnSelect: true,
+     rtl: $rtl,
+     prevArrow: '<div class="slick-prev mt-radius-b"></div>',
+     nextArrow: '<div class="slick-next mt-radius-b"></div>',
+     responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+        centerPadding: '0px',
+        }
+      }]
+  });
+  jQuery('.post-carousel').slick({
+       slidesToShow: 4,
+       variableWidth: true,
+       lazyLoad: 'ondemand',
+       autoplay: $autoplay,
+       autoplayTimeout:5000,
+       speed:450,
+       rtl: $rtl,
+       prevArrow: '<div class="poster-prev mt-radius"></div>',
+       nextArrow: '<div class="poster-next mt-radius"></div>',
+       responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            variableWidth: false,
+            autoplay: false,
+          }
+        }]
+    });
+});
